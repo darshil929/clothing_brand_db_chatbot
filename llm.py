41ebd967a5122a54f4e43c66fdd7f5afbcc88f0a
darshil929 <darshilt929@gmail.com>
2024-07-15 03:55:27 +0530

    initialized db chain

--- llm.py
+++ llm.py
@@ -15,3 +15,11 @@ db_user, db_password, db_host, db_name = info.get_db_info()
 db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",sample_rows_in_table_info=3)
 
 print(db.table_info)
+
+# database chain
+db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+# test query
+qns1 = db_chain("How many t-shirts do we have left for nike in extra small size and white color?")
+
+qns2 = db_chain.run("How much is the price of the inventory for all small size t-shirts?")
