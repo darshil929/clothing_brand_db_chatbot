7eebf3616b685da74d1e25dc75d53c7519d20719
darshil929 <darshilt929@gmail.com>
2024-07-15 03:55:02 +0530

    database connected

--- llm.py
+++ llm.py
@@ -8,3 +8,10 @@ info = Info()
 api_key = info.get_api_key()
 
 llm = GooglePalm(google_api_key=api_key, temperature=0.2)
+
+# database connection
+db_user, db_password, db_host, db_name = info.get_db_info()
+
+db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",sample_rows_in_table_info=3)
+
+print(db.table_info)
