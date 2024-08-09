0f62d907f0b040fb1c255212f3b55f96b38dc7b3
darshil929 <darshilt929@gmail.com>
2024-08-09 16:18:50 +0530

    vectorstore

--- llm.py
+++ llm.py
@@ -1,6 +1,9 @@
 from langchain.llms import GooglePalm
 from langchain.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
+from langchain.prompts import SemanticSimilarityExampleSelector
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
 
 from info import Info
 from few_shots import few_shots
@@ -42,3 +45,19 @@ qns4 = db_chain.run("SELECT SUM(price * stock_quantity) FROM t_shirts WHERE bran
 qns5 = db_chain.run("How many white color Levi's t shirts we have available?")
 
 qns5 = db_chain.run("SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'")
+
+### Few Shot Learning
+
+# generating embeddings
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+to_vectorize = [" ".join(example.values()) for example in few_shots]
+
+vectorstore = Chroma.from_texts(to_vectorize, embeddings, metadatas=few_shots)
+
+example_selector = SemanticSimilarityExampleSelector(
+    vectorstore=vectorstore,
+    k=2,
+)
+
+example_selector.select_examples({"Question": "How many Adidas T shirts I have left in my store?"})
